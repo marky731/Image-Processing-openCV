5e14956097d5c01f70f1e9ce669379f9f880c4ad
ikram uygur <123154240+marky731@users.noreply.github.com>
2023-12-23 19:05:07 +0300

    WEEK_23 (shape detection code will be rewritten)

--- shape_detection_shortVersion.py
+++ shape_detection_shortVersion.py
@@ -3,4 +3,4 @@ import ultralytics
 
 model = ultralytics.YOLO("2023.12.23/shape_detection.pt") 
 
-results = model(source=0, show=True, conf=0.4)
+results = model(source=0, show=True, conf=0.4) # displays the detection on real time
